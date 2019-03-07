4947d029a60a55e2b14d2f54383b3f15f68211f7
Eric Nantz <thercast@gmail.com>
2019-03-07 16:51:25 -0500

    update lego dev script (messy)

--- dev/lego_dev.R
+++ dev/lego_dev.R
@@ -7,20 +7,41 @@ res <- scale_image(image, img_size = 48) %>%
   legoize() %>%
   collect_bricks(num_steps = 8)
 
+
+# analyze metrics
+pieces <- res$pieces %>%
+  select(Brick_size) %>%
+  distinct() %>%
+  pull() %>%
+  length()
+
+pieces_cost <- res$pieces %>%
+  group_by(Brick_size) %>%
+  summarize(n_total = sum(n)) %>%
+  ungroup %>%
+  left_join(
+    price_data,
+    by = c("Brick_size" = "size")
+  ) %>%
+  mutate(brick_type_cost = n_total * price)
+
+data("price_data")
+  
+
 display_set(res)
 
 res_steps <- generate_steps(res$Img_bricks, num_steps = 10)
 
 pcs_step <- count_bricks(res_steps, "Step 03")
 
-
-
-
 # unique steps in the steps data frame
 step_ids <- unique(res_steps$Step)
 
 pcs_steps_all <- step_pieces(res_steps)
 pcs_step <- step_pieces(res_steps, "Step 02")
+
+table_pieces(res$pieces)
+mytable <- table_pieces(pcs_steps_all, "Step 02")
 
 plot_instructions(res_steps)
 plot_instructions(res_steps, step_id = "Step 01")
